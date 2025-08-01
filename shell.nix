675fccf962b32485ecf2823789848ce514109d40
Karel Šebela <karelsebela@protonmail.com>
2025-08-01 16:41:47 +0200

    shell.nix whatever

--- shell.nix
+++ shell.nix
@@ -1,7 +1,13 @@
-with import <nixpkgs> {};
-mkShell {
+{ pkgs ? import <nixpkgs> {} }:
+# {mkShell,qt6,libglvnd}:
+pkgs.mkShell {
+# pkgs.stdenv.mkDerivation {
+  name = "dontcare";
   buildInputs = [
-    kdePackages.qtbase
-    kdePackages.qtdeclarative
+    # kdePackages.qtbase
+    # kdePackages.qtdeclarative
+    # kdePackages.qtquick3d
+    pkgs.qt6.full
+    pkgs.libglvnd
   ];
 }
